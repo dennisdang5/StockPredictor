28ce13fcd436eac88e629ab180dab95fddbb5c9f
Logan Yamamoto <lkyamamo@discovery2.hpc.usc.edu>
2025-09-21 15:53:08 -0700

    adding submission script

--- logan-version/main.py
+++ logan-version/main.py
@@ -1,10 +1,19 @@
 import train
-import torch
 
-trainer = train.Trainer()
+
+start = "1989-12-01"
+end = "2015-09-30"
+stocks = ["AAPL","MSFT"]
+trainer = train.Trainer(stocks=stocks, time_args=[start,end])
+
+for name, param in trainer.lstmModel.named_parameters():
+    print("name: {}".format(name))
+    print("param: {}".format(param.numel()))
+    print()
 
 for epoch in range(trainer.num_epochs):
     stop = trainer.train_one_epoch(epoch)
     if stop: 
         print("Early stop at epoch: {}".format(epoch))
-        trainer.writer.close()
+        trainer.stop()
+        break
